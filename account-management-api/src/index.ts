068e0e577a8b3859cc1e60ec205d5c1065f62ee6
St Dev <st.dev321@gmail.com>
2023-07-05 21:32:48 +0530

    removed unwanted code

--- account-management-api/src/index.ts
+++ account-management-api/src/index.ts
@@ -13,10 +13,6 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('<h1>Hello from the TypeScript world!</h1>');
-});
-
 // Routes
 app.use('/accounts', accountRouter);
 
